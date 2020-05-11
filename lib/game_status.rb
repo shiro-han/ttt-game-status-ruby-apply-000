e03463afd36dd2c95e256ea72c09c41f748d5cf7
Shiro Han <shan99business@gmail.com>
2020-05-11 04:00:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true || full? board == true
+  if won?(board) == true || draw?(board) == true || full?(board) == true
     true
   else
     false
